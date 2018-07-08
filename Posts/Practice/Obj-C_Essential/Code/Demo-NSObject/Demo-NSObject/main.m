435d2806fa85c701685a2aadc6a88b63bac701a3
kingcos <2821836721v@gmail.com>
2018-07-09 02:07:13 +0800

    New Demo-NSObject

---
+++ Posts/Practice/Obj-C_Essential/Code/Demo-NSObject/Demo-NSObject/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Demo-NSObject
+//
+//  Created by kingcos on 2018/7/9.
+//  Copyright © 2018 kingcos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
