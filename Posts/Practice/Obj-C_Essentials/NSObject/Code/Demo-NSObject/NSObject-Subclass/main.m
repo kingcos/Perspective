05d0215251edc4a46a9b9877fbf0d58d4404d9cd
kingcos <2821836721v@gmail.com>
2018-07-11 01:53:14 +0800

    Add new NSObject subclass target

---
+++ Posts/Practice/Obj-C_Essentials/NSObject/Code/Demo-NSObject/NSObject-Subclass/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NSObject-Subclass
+//
+//  Created by kingcos on 2018/7/11.
+//  Copyright © 2018 kingcos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
