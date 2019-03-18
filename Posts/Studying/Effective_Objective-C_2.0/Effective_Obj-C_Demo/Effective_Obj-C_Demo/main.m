54ada84bc50d9245ca40baf5e30415a95498ce4c
kingcos <2821836721v@gmail.com>
2019-03-18 17:25:21 +0800

    Add demo project

---
+++ Posts/Studying/Effective_Objective-C_2.0/Effective_Obj-C_Demo/Effective_Obj-C_Demo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Effective_Obj-C_Demo
+//
+//  Created by kingcos on 2019/3/18.
+//  Copyright © 2019 kingcos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
