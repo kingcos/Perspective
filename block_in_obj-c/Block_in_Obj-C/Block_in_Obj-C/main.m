675af006b94f3a80bb340dfb334c20e81e2f5258
kingcos <2821836721v@gmail.com>
2019-11-24 12:54:45 +0800

    Add Block_in_Obj-C demo

---
+++ block_in_obj-c/Block_in_Obj-C/Block_in_Obj-C/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Block_in_Obj-C
+//
+//  Created by kingcos on 2019/11/23.
+//  Copyright © 2019 kingcos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
